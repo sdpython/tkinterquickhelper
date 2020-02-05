ceb917f1d5eda45dab723d357bc58b91e1a538a9
xavier dupré <xavier.dupre@gmail.com>
2020-02-05 23:26:04 +0100

    add support for pytohn 3.8

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -10,12 +10,14 @@ from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
-set_sphinx_variables(__file__, "tkinterquickhelper", "Xavier Dupré", 2019,
+set_sphinx_variables(__file__, "tkinterquickhelper", "Xavier Dupré", 2020,
                      "solar_theme", solar_theme.theme_path, locals(),
                      github_repo="https://github.com/sdpython/tkinterquickhelper.git",
                      extlinks=dict(issue=(
                          'https://github.com/sdpython/tkinterquickhelper/issues/%s', 'issue {0} on GitHub')),
                      link_resolve="http://www.xavierdupre.fr/app/")
+
+html_logo = "phdoc_static/project_ico.png"
 
 # there is an issue with this attribute on Anaconda math_number_all
 assert math_number_all or not math_number_all
